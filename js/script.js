031f7685d16c3131b091e208819787b16602128a
nixon2105 <radivilov1991@mail.ru>
2020-03-15 19:28:51 +0300

    feat: add fanctions addTagsClickHandler and selectClikedTag

--- js/script.js
+++ js/script.js
@@ -11,9 +11,7 @@ const anchors = document.querySelectorAll('a.navigation__link')
 for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
     e.preventDefault()
-    
     const blockID = anchor.getAttribute('href');
-    
     document.querySelector(blockID).scrollIntoView({
       behavior: 'smooth',
       block: 'start'
@@ -21,5 +19,30 @@ for (let anchor of anchors) {
   });
 };
 
+//slider
 
+const addTagsClickHandler = () => {
+    document.querySelector('.navigation-portfolio').addEventListener('click', (e) => {
+        e.preventDefault()
+        if (e.target.classList.contains('navigation-portfolio__link')) {
+            let clickedTag = e.target;
+            removeActiveTags();
+            selectClikedTag(clickedTag);
+        }
+    })
+}
+
+const removeActiveTags = () => {
+    let tags = document.querySelectorAll('.navigation-portfolio__link');
+    tags.forEach(tag => {
+        tag.classList.remove('active');
+        tag.classList.add('navigation-portfolio__link');
+    })
+}
+
+const selectClikedTag = (clickedTag) => {
+    clickedTag.classList.add('active');
+}
+
+addTagsClickHandler();
 
